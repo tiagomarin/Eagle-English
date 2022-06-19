b87bcff7c99a2ae33238ff934caccb0a3e64b691
Tiago Marin <tiago.lelinski@gmail.com>
2022-06-19 02:55:10 -0300

    forgot to save before last commit

--- burger-menu.js
+++ burger-menu.js
@@ -1,12 +1,15 @@
 const openMenuBtn = document.querySelector('.menu-btn');
 const clsMenuBtn = document.querySelector('.close-menu-btn');
 const mobMenu = document.querySelector('.mobile-menu');
+const body = document.body;
 
 openMenuBtn.addEventListener('click', function() {
   mobMenu.classList.add('display-block');
+  body.classList.add('modal-on')
 });
 clsMenuBtn.addEventListener('click', function() {
   mobMenu.classList.remove('display-block');
+  body.classList.remove('modal-on')
 });
 
 // function openModal(element) {
