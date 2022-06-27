1064f301cd4d8488c42cad72e70433bd1f73b5c1
Tiago Marin <tiago.lelinski@gmail.com>
2022-06-27 10:53:49 -0300

    set up scss files & change nested array to array of objects

--- script.js
+++ script.js
@@ -11,37 +11,108 @@ function random(min, max) {
 
 // create a teachers database
 const teachersDatabase = [
-  ['assets/tiagoMarin.jpeg', 'Tiago Marin', 'Graduated in business, worked in multinational companies and lived in different countries', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi assumenda quod at facere incidunt fuga aperiam quaerat quo dolorum?'],
-  ['image.png', 'name2', 'higlights2', 'description2'],
-  ['image.png', 'name3', 'higlights3', 'description3'],
-  ['image.png', 'name4', 'higlights4', 'description4'],
-  ['image.png', 'name5', 'higlights5', 'description5'],
-  ['image.png', 'name6', 'higlights6', 'description6'],
-  ['image.png', 'name7', 'higlights7', 'description7'],
-  ['image.png', 'name8', 'higlights8', 'description8'],
-  ['image.png', 'name9', 'higlights9', 'description9'],
-  ['image.png', 'name6', 'higlights6', 'description10'],
-  ['image.png', 'name7', 'higlights7', 'description11'],
-  ['image.png', 'name8', 'higlights8', 'description12'],
-  ['image.png', 'name9', 'higlights9', 'description13'],
-  ['image.png', 'name6', 'higlights6', 'description14'],
-  ['image.png', 'name7', 'higlights7', 'description15'],
-  ['image.png', 'name8', 'higlights8', 'description16'],
-  ['image.png', 'name9', 'higlights9', 'description17']
+  {
+    image: 'assets/pictures/1.png',
+    name: 'Tiago Marin',
+    highlight: 'Graduated in business, worked in multinational companies and lived in different countries',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quasi assumenda quod at facere incidunt fuga aperiam quaerat quo dolorum?'
+  },
+  {
+    image: 'assets/pictures/2.png',
+    name: 'name2',
+    highlight: 'higlights2',
+    description: 'description2'
+  },
+  {
+    image: 'assets/pictures/3.png',
+    name: 'name3',
+    highlight: 'higlights3',
+    description: 'description4'
+  },
+  {
+    image: 'assets/pictures/4.png',
+    name: 'name4',
+    highlight: 'higlights4',
+    description: 'description4'
+  },
+  {
+    image: 'assets/pictures/5.png',
+    name: 'name5',
+    highlight: 'higlights5',
+    description: 'description5'
+  },
+  {
+    image: 'assets/pictures/6.png',
+    name: 'name6',
+    highlight: 'higlights6',
+    description: 'description6'
+  },
+  {
+    image: 'assets/pictures/7.png',
+    name: 'name7',
+    highlight: 'higlights7',
+    description: 'description7'
+  },
+  {
+    image: 'assets/pictures/8.png',
+    name: 'name8',
+    highlight: 'higlights8',
+    description: 'description8'
+  },
+  {
+    image: 'assets/pictures/9.png',
+    name: 'name9',
+    highlight: 'higlights9',
+    description: 'description9'
+  },
+  {
+    image: 'assets/pictures/10.png',
+    name: 'name10',
+    highlight: 'higlights10',
+    description: 'description10'
+  },
+  {
+    image: 'assets/pictures/11.png',
+    name: 'name11',
+    highlight: 'higlights11',
+    description: 'description11'
+  },
+  {
+    image: 'assets/pictures/12.png',
+    name: 'name12',
+    highlight: 'higlights12',
+    description: 'description12'
+  },
+  {
+    image: 'assets/pictures/13.png',
+    name: 'name13',
+    highlight: 'higlights13',
+    description: 'description13'
+  },
+  {
+    image: 'assets/pictures/14.png',
+    name: 'name14',
+    highlight: 'higlights14',
+    description: 'description14'
+  }
 ];
 // ps. data base could increase overtime
 
- // create featured teachers database by SHUFFLING through the teachersDatabase
- let featTeachersDatabase = [];
- let iBefore = 0;
- let ramdonIndex = 0;
- for (let j = 0; j < 6; j++) {
-   let ramdonIndex = random(0, teachersDatabase.length);
-   if (ramdonIndex !== iBefore) {
-     featTeachersDatabase.push(ramdonIndex);
-   }
-   iBefore = ramdonIndex;
- }
+
+// create array with 5 ramdon integers
+//to be used as index to SHUFFLE through the teachersDatabase
+
+let sixRamdonIndexes = [];
+sixRamdonIndexes.push(random(0, 14));
+
+while (sixRamdonIndexes.length < 6) {
+  let ramdonIndex = random(0, 14);
+  if (sixRamdonIndexes.includes(ramdonIndex) == false) {
+    sixRamdonIndexes.push(ramdonIndex);
+    console.log(sixRamdonIndexes);
+  }
+};
+
 // ----------------
 
 
@@ -65,7 +136,7 @@ featTeachers.appendChild(ul);
 
 for (let i = 0; i < 6; i++) {
 
- 
+
   let li = document.createElement('li');
   li.id = 'teachers-card';
   ul.appendChild(li);
@@ -74,24 +145,29 @@ for (let i = 0; i < 6; i++) {
 
   // card image
   let img = document.createElement('img');
-  img.src = teachersDatabase[featTeachersDatabase[i]][0];
+  img.src = teachersDatabase[sixRamdonIndexes[i]].image;
   li.appendChild(img);
+
+  //div for card info
+  let div = document.createElement('div');
+  div.className = 'card-info';
+  li.appendChild(div);
 
   // heading - teacher NAME
   let h3 = document.createElement('h3');
   h3.className = 'card-heading';
-  h3.textContent = teachersDatabase[featTeachersDatabase[i]][1];
-  li.appendChild(h3);
+  h3.textContent = teachersDatabase[sixRamdonIndexes[i]].name;
+  div.appendChild(h3);
 
   // sub-heading "teacher highlight"
   let p = document.createElement('p');
   p.className = 'teacher-highlight';
-  p.textContent = teachersDatabase[featTeachersDatabase[i]][2];
-  li.appendChild(p);
+  p.textContent = teachersDatabase[sixRamdonIndexes[i]].highlight;
+  div.appendChild(p);
 
   //short description
   p = document.createElement('p');
   p.className = 'card-para';
-  p.textContent = teachersDatabase[featTeachersDatabase[i]][3];
-  li.appendChild(p);
+  p.textContent = teachersDatabase[sixRamdonIndexes[i]].description;
+  div.appendChild(p);
 }
